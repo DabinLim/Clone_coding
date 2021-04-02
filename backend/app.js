e70b44e2f504a7e627b756defe571abeae08196a
=danaisboss <yjk9313@gmail.com>
2021-04-02 17:08:54 +0900

    Add Write controller(CRUD관련)

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { globalRouter } from "./routes/globalRoute.js";
 const app = express();
-export default app;
 
 
 app.use(express.json());
 app.use('/public', express.static('public'));
 app.use('/', globalRouter);
+
+
+export default app;
---
+++ backend/controller/postController.js
@@ -0,0 +1,15 @@
+export const postWrite = async (req, res) => {
+  res.send('postWrite');
+};
+
+export const getWrite = async (req, res) => {
+  res.send('getWrite');
+};
+
+export const getEditWrite = async (req, res) => {
+  res.send('getEdit write');
+};
+
+export const postEditWrite = async (req, res) => {
+  res.send('post Edit write');
+};
