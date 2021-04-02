8caa275e1b639a64679b6e1f77b470c9692a3e47
=danaisboss <yjk9313@gmail.com>
2021-04-02 17:55:57 +0900

    글쓰기 관련 Route, controller 추가

--- backend/routes/globalRoute.js
+++ backend/routes/globalRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { home } from "../controller/homeController.js";
-import { getWrite, postWrite } from "../controller/postController.js";
+import { deleteWrite, detail, getEditWrite, getWrite, postEditWrite, postWrite } from "../controller/postController.js";
 export const globalRouter = express.Router();
 
 
 globalRouter.get('/', home);
 globalRouter.get('/write', getWrite);
 globalRouter.post('/write', postWrite);
+globalRouter.get('/detail', detail);
+globalRouter.get('/detail/:id/edit', getEditWrite);
+globalRouter.post('/detail/:id/edit', postEditWrite);
+globalRouter.delete('/detail/:id/delete', deleteWrite);
