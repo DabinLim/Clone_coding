79bfab8b85b78f369a21f77c20275231e1c9c0e7
=danaisboss <yjk9313@gmail.com>
2021-04-02 15:52:27 +0900

    Add init.js

---
+++ backend/init.js
@@ -0,0 +1,10 @@
+import app from "../backend/app.js"
+
+
+const PORT = 3000;
+
+const handleListning = () => {
+  console.log(`✅Listning on http://localhost:${PORT}`)
+}
+
+app.listen(PORT, handleListning);
