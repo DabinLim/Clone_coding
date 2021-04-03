877c97c70e5654085f15f5100887979c9a1a0fd4
=danaisboss <yjk9313@gmail.com>
2021-04-03 11:37:33 +0900

    node x react 연동 연습

---
+++ BF/myexample/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+const port =process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', (req, res)=> res.json({username:'yeonjae'}));
+
+app.listen(port, ()=>{
+    console.log(`express is running on ${port}`);
+})
