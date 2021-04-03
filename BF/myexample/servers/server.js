bedb769cfa5c60b7c6fbc4c901de54ce5848ff58
=danaisboss <yjk9313@gmail.com>
2021-04-03 13:32:07 +0900

    node x react 연동, with proxy

--- BF/myexample/servers/server.js
+++ BF/myexample/servers/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors'); 기존에 서버를 두개 하나하나 켜고 cors해줘야했지만 이제 필요없습니다.
 const app = express();
 const bodyParser = require('body-parser');
 const port =process.env.PORT || 3001;
 
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', (req, res)=> res.json({username:'yeonjae'}));
+app.use('/api', (req, res)=> 
+  res.json({username:'yeonjae'}));
 
 app.listen(port, ()=>{
     console.log(`express is running on ${port}`);
