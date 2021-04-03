36c4486fb73f9ebf8e0702144f6e6961bb55e4b3
=danaisboss <yjk9313@gmail.com>
2021-04-03 14:28:07 +0900

    detail 페이지 코드 작성

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -14,11 +14,23 @@ const postUpload = async (req, res) => {
   res.redirect(`detail/:${newPost.id}`)
 };
 
+//글쓰기 화면 랜더링
 const getUpload = async (req, res) => {
   res.render('view file 이름');
 };
 
+//상세페이지 랜더링
 const detail = async (req, res) => {
+  const {
+    params: {id},
+  } = req;
+//TODO: detail 페이지 랜더링 파일 수정해야함. 임의로 넣어놓은값임
+  try {
+    const post = await Post.findById(id).populate('comments');
+    res.render(`detail`, { post }); 
+  } catch (error) {
+
+  }
   res.send('detail page');
 };
 
